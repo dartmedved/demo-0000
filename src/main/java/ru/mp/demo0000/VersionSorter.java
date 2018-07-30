18a891bbd96f848a9912a86c5cbfa4e1af53aafa
Mike Pustovalov <dartbear@gmail.com>
2018-07-31 01:33:21 +0300

    + first task complete

---
+++ src/main/java/ru/mp/demo0000/VersionSorter.java
@@ -0,0 +1,23 @@
+package ru.mp.demo0000;
+
+import lombok.experimental.UtilityClass;
+import org.apache.maven.artifact.versioning.ComparableVersion;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@UtilityClass
+public class VersionSorter {
+    public List<String> sortVersions(final List<String> unsortedVersions){
+        if(unsortedVersions==null || unsortedVersions.isEmpty()){
+            return Collections.emptyList();
+        }
+
+        return unsortedVersions.stream()
+                .map(ComparableVersion::new)
+                .sorted()
+                .map(ComparableVersion::toString)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/ru/mp/demo0000/VersionSorterTest.java
@@ -0,0 +1,54 @@
+package ru.mp.demo0000;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.junit.Assert.assertThat;
+
+@Slf4j
+public class VersionSorterTest {
+    @Test
+    public void testEmptySort() {
+        {
+            final List<String> result = VersionSorter.sortVersions(null);
+            assertThat(result, is(notNullValue()));
+            assertThat(result.size(), is(equalTo(0)));
+        }
+        {
+            final List<String> result = VersionSorter.sortVersions(Collections.emptyList());
+            assertThat(result, is(notNullValue()));
+            assertThat(result.size(), is(equalTo(0)));
+        }
+    }
+
+    @Test
+    public void sampleSort() {
+        final List<String> given = Arrays.asList("3.0", "3.0.13", "1.2.3", "2.4.31", "1.0", "1.2");
+        log.info("Given versions list : {}", given);
+        final List<String> result = VersionSorter.sortVersions(given);
+        log.info("Sorted versions list : {}", result);
+        assertThat(result, is(notNullValue()));
+        assertThat(result.size(), is(equalTo(given.size())));
+        assertThat(result,contains("1.0", "1.2", "1.2.3", "2.4.31", "3.0", "3.0.13"));
+    }
+
+    @Test
+    public void sampleSortWithSnapshot() {
+        final List<String> given = Arrays.asList("3.0", "3.0.13", "1.2.3", "2.4.31", "1.0", "1.2",
+                "1.0-SNAPSHOT", "1.2-SNAPSHOT");
+        log.info("Given versions list : {}", given);
+        final List<String> result = VersionSorter.sortVersions(given);
+        log.info("Sorted versions list : {}", result);
+        assertThat(result, is(notNullValue()));
+        assertThat(result.size(), is(equalTo(given.size())));
+        assertThat(result,contains("1.0-SNAPSHOT", "1.0", "1.2-SNAPSHOT", "1.2", "1.2.3", "2.4.31", "3.0", "3.0.13"));
+    }
+}
